978123b665ec2d821c67824d1ecc506f699f0b74
YunmiSeo <YunmiSeo@users.noreply.github.com>
2020-07-16 12:58:25 +0200

    get elements

---
+++ script.js
@@ -0,0 +1,6 @@
+const main = document.getElementById("main");
+const userBtn = document.getElementById("user");
+const doubleBtn = document.getElementById("double");
+const showBtn = document.getElementById("show-mio");
+const sortBtn = document.getElementById("sort");
+const calBtn = document.getElementById("calculate-wealth");
