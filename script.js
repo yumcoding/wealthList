e39484658eb1addd6ebafdf717b647795a4d4f1a
YunmiSeo <YunmiSeo@users.noreply.github.com>
2020-07-16 13:18:40 +0200

    get Ranom user and money
    use 3rd party api
    https://randomuser.me/api

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ const doubleBtn = document.getElementById("double");
 const showBtn = document.getElementById("show-mio");
 const sortBtn = document.getElementById("sort");
 const calBtn = document.getElementById("calculate-wealth");
+
+let data = [];
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
+//get random users and their money
+
+async function getRandomUser() {
+  const res = await fetch("https://randomuser.me/api");
+  const data = await res.json();
+  const user = data.results[0];
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000),
+  };
+  //   console.log(newUser);
+
+  addData(newUser);
+}
